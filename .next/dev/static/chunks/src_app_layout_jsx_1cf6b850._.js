7fab09e8b440cb1d1cb47303a6ce3ca1d3947151
Mark W <mw@Marks-MacBook-Pro.local>
2026-03-05 08:58:08 +0000

    chore: stash nextjs build files

--- .next/dev/static/chunks/src_app_layout_jsx_1cf6b850._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_c05b8343._.js",
-  "static/chunks/src_components_78775029._.js",
-  "static/chunks/[root-of-the-server]__a8d598fa._.css"
-],
-    source: "dynamic"
-});
